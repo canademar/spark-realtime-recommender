39c0af9c8b8c15a17857a50d35a6cb6c25eb3695
cnavarro <cnavarro@paradigmatecnologico.com>
2017-11-23 16:46:12 +0100

    Commented old line

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
-~/spark-1.3.1-bin-hadoop2.6/bin/spark-submit --master spark://169.254.206.2:7077 --total-executor-cores 14 --executor-memory 6g server.py 
+#~/spark-1.3.1-bin-hadoop2.6/bin/spark-submit --master spark://169.254.206.2:7077 --total-executor-cores 14 --executor-memory 6g server.py 
 spark-submit --master local[*] server.py
